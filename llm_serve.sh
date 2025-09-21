0ef4aa9e4a44bcb3d7745d90406e8d080a421060
Danis Zaripov <daniszaripov02@mail.ru>
2025-09-22 00:08:53 +0300

    init

---
+++ llm_serve.sh
@@ -0,0 +1,1 @@
+python3 -m sglang.launch_server --model "Qwen/Qwen3-4B-Thinking-2507-FP8" --mem-fraction-static 0.85 --chunked-prefill-size -1 --max-prefill-tokens 1024 --enable-torch-compile --max-running-requests 32 --max-total-tokens 16384 --random-seed 42 --host 0.0.0.0 --port 30000
